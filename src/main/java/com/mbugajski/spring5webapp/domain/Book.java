46c51afb69071b35288ba3fc951191747e36e73b
MBugajski <michalbugajski@outlook.com>
2020-12-04 17:13:16 +0100

    Thymeleaf and H2

--- src/main/java/com/mbugajski/spring5webapp/domain/Book.java
+++ src/main/java/com/mbugajski/spring5webapp/domain/Book.java
@@ -105,7 +105,6 @@ public class Book {
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
-                ", authors=" + authors +
                 '}';
     }
 }
